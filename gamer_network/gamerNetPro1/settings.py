4e4865d3153627f26e12ab4b1c441bd1cb227820
Jed Boyle <jboyle1@pca.ac.uk>
2020-02-20 12:28:39 +0000

    Added extra static config in settings.py

--- gamer_network/gamerNetPro1/settings.py
+++ gamer_network/gamerNetPro1/settings.py
@@ -119,14 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
